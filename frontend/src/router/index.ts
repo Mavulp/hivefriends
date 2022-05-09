af2b84f8b4e9ffb4e94fd5cf58b407e40c71a6e3
Jan Dolanský <dolanovsky@gmail.com>
2022-05-09 16:16:59 +0300

    Setup empty routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import Login from "./views/Login.vue"
 import Home from "./views/Home.vue"
+import AlbumDetail from "./views/AlbumDetail.vue"
+import ImageDetail from "./views/ImageDetail.vue"
+import Albums from "./views/Albums.vue"
 
+/**
+ * Router Setup
+ */
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -25,10 +31,39 @@ const router = createRouter({
       meta: {
         title: "Home"
       }
+    },
+    {
+      path: "/album/all",
+      name: "Albums",
+      component: Albums,
+      meta: {
+        title: "All Albums"
+      }
+    },
+    {
+      path: "/album/:id",
+      name: "Albums",
+      component: AlbumDetail,
+      meta: {
+        // This will be updated on route load
+        title: "Album Detail"
+      }
+    },
+    {
+      path: "/album/:id/image/:image",
+      name: "Albums",
+      component: ImageDetail,
+      meta: {
+        // This will be also update
+        title: "Image Detail"
+      }
     }
   ]
 })
 
+/**
+ * Router Guards
+ */
 router.afterEach((to) => {
   document.title = `${to.meta.title} :: hi!friends`
 })
---
+++ frontend/src/router/views/Albums.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { ref } from "vue"
+</script>
+
+<template>
+  <div>every albuim areber</div>
+</template>
