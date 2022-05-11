9da77839b6df12ef58cca091dbdbb1aee9282b0c
Jan Dolanský <dolanovsky@gmail.com>
2022-05-11 14:13:11 +0300

    Remove import of google icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,4 +4,14 @@ import App from "./App.vue"
 import router from "./router"
 
 const app = createApp(App).use(createPinia()).use(router)
+
+// Init icons
+// import PhotoLibrary from "vue-material-design-icons/PhotoLibraryIcon.vue"
+// import Settings from "vue-material-design-icons/SettingsIcon.vue"
+// import Logout from "vue-material-design-icons/LogoutIcon.vue"
+
+// app.component("icon-photo-library", PhotoLibrary)
+// app.component("icon-settings", Settings)
+// app.component("icon-logout", Logout)
+
 app.mount("#app")
