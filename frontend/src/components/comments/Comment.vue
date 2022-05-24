c0c80fdd23b44fb974a46490fd1b58aa3b5fcc2e
Jan Dolanský <dolanovsky@gmail.com>
2022-05-25 01:48:16 +0300

    ?

--- frontend/src/components/comments/Comment.vue
+++ frontend/src/components/comments/Comment.vue
@@ -21,7 +21,10 @@ const props = defineProps<Props>()
   <div class="hi-comment">
     <div class="comment-header">
       <button
-        v-if="props.uploader === props.data.author && props.uploader === user.user.username"
+        v-if="
+          (props.uploader === props.data.author && props.uploader === user.user.username) ||
+          props.uploader === user.user.username
+        "
         class="control-button hover-bubble"
         data-title-top="Remove"
         @click="comments.delComment(props.imageKey, data.id)"
