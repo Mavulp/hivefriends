8d63a22015cf32143f0a31dde423328d17961590
Floris Vastenhout <f.vastenhout@.live.nl>
2022-06-05 17:36:54 +0200

    Test clippy action warning

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -26,6 +26,8 @@ async fn main() {
 }
 
 async fn run() -> anyhow::Result<()> {
+    let foo = 0;
+
     let args: cli::Args = argh::from_env();
 
     let db_path: PathBuf = std::env::var("DB_PATH").context("DB_PATH not set")?.into();
