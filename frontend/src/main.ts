b78343c778122069d58f860f87a2536e7f32080a
dolanske <dolanovsky@gmail.com>
2022-06-05 21:01:36 +0200

    No head?

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,12 @@
 import { createPinia } from "pinia"
 import { createApp } from "vue"
-// import { createHead } from "@vueuse/head"
 
 import App from "./App.vue"
 import router from "./router"
 
-// const head = createHead()
 const app = createApp(App)
 
 app.use(createPinia())
-// app.use(head)
 app.use(router)
 
 app.mount("#app")
