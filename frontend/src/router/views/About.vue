79cc21176f481a19ca6cec88b5e3f41169a0ac2a
dolanske <dolanovsky@gmail.com>
2022-06-11 15:02:05 +0200

    Fix album line clamp. Update about language
    Resolves #55

--- frontend/src/router/views/About.vue
+++ frontend/src/router/views/About.vue
@@ -14,19 +14,24 @@ onBeforeMount(() => {
     <div class="container">
       <h1>The project</h1>
       <p>
-        This project was created in collaboration of friends from the hivecom community, which also formed its own
-        development team called <a target="_blank" href="https://github.com/mavulp">Mavulp</a>. The goal for us was to
-        create a centralized hub to share albums or iamges mostly for real life events or meetings. <br />
-        <br />
-
+        This project was created as a collaboration of friends from the Hivecom community and the dedicated development
+        team called <a target="_blank" href="https://github.com/mavulp">Mavulp</a>. The goal of Hi!Friends is to create
+        a centralized hub to share images of mostly real life gatherings, events and adventures we embark on together.
         Given the fact we've all grown in different parts of the world and some of us have knonw each other for over 10
         years, these albums can hold a lot of sentimntal value.
 
         <br />
         <br />
-        <br />
 
-        We also do a lot of gaming together, want to join? Check out our site
+        Given the fact that we - the community behind Hivecom and it's further groups of friends - all grew up in
+        different parts of the world and in the course of over 10 years found together through our shared interests,
+        goals and quite simply bizarre circumstances; these albums can hold a lot of sentimental value that can only be
+        described as a family bond.
+
+        <br />
+        <br />
+        We'd like to share this bond between all of us, and all of you who might one day be a part of that too. In the
+        meantime: we also do a lot of gaming together, want to join? Check out our site!
       </p>
 
       <a target="_blank" href="https://hivecom.net/" class="link-out">
@@ -46,6 +51,31 @@ onBeforeMount(() => {
         <a target="_blank" href="https://github.com/Mavulp/hivefriends/issues">Github issue</a> or send us a message on
         <a target="_blank" href="https://discord.com/invite/Uh4WJ6vV">Discord</a> in the <strong>#chat</strong> channel.
       </p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <h4>Support us</h4>
+
+      <p>
+        Running all the servers and storage adds to cost each month. We do all the financing out of our own pockets. If
+        you want to support us, feel free to donate a small amount to our
+        <a href="https://ko-fi.com/hivecom">ko-fi</a> or join our patreon.
+      </p>
+
+      <a target="_blank" href="https://www.patreon.com/hivecom" class="link-out">
+        <div>
+          <strong>Hivecom on Patreon</strong>
+          <p>https://www.patreon.com/hivecom</p>
+        </div>
+
+        <img
+          src="https://i0.wp.com/jamesgarside.net/wp-content/uploads/2019/03/e04cb391687ead757f671bc977b9de83.jpg"
+          alt=""
+        />
+      </a>
 
       <p class="copyright">
         <span class="material-icons"> &#xe86f; </span>
